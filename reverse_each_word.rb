65e1f193c975d340abe4653cfc1181f1a8752cfb
Mai Pham <maitphm@gmail.com>
2020-10-30 20:06:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 # reverse_each_word("Hello there, and how are you?")
 
-def reverse_word(string)
+def reverse_each_word(string)
   word = string.split(" ")
  
   backwards = word.collect { |letter| letter.reverse}
